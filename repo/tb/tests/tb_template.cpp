a5f35fa1a1d35325652eb803ad0bd6975a6921fd
MAadinP <m.aadinpatel@gmail.com>
2024-11-21 15:47:56 +0000

    Brown EQ Test Successful

---
+++ repo/tb/tests/tb_template.cpp
@@ -0,0 +1,102 @@
+/*
+ *  Verifies the results of the mux, exits with a 0 on success.
+ */
+
+#include "base_testbench.h"
+
+Vdut *top;
+VerilatedVcdC *tfp;
+unsigned int ticks = 0;
+
+class BrownTestbench : public BaseTestbench
+{
+protected:
+    void initializeInputs() override
+    {
+        top->clk = 1;
+    }
+
+    void runEQSimulation(int cycles = 1)
+    {
+        for (int i = 0; i < cycles; i++)
+        {
+            for (int clk = 0; clk < 2; clk++)
+            {
+                top->eval();
+                tfp->dump(2 * ticks + clk);
+                top->clk = !top->clk;
+            }
+            ticks++;
+
+            if (Verilated::gotFinish())
+            {
+                exit(0);
+            }
+        }
+    }
+
+    void runLOADSimulation(int cycles = 1)
+    {
+        for (int i = 0; i < cycles; i++)
+        {
+            for (int clk = 0; clk < 2; clk++)
+            {
+                top->eval();
+                tfp->dump(2 * ticks + clk);
+                top->clk = !top->clk;
+            }
+            ticks++;
+
+            if (Verilated::gotFinish())
+            {
+                exit(0);
+            }
+        }
+    }
+
+};
+
+TEST_F(BrownTestbench, EQWORKS)
+{
+    top->rs1 = 0;
+    top->rs2 = 0;
+    top->ALUsrc = 0;
+    top->ALUctrl = 0;
+    top->RegWrite = 0;
+
+    runEQSimulation(1);
+    EXPECT_EQ(top->EQ, 1);
+}
+
+// TEST_F(BrownTestbench, LOADIMMWORKS)
+// {
+
+//     top->ALUsrc = 1;
+//     top->ALUctrl = ;
+//     top->RegWrite = 0;
+    
+//     top->eval();
+
+//     EXPECT_EQ(top->out, 1);
+// }
+
+int main(int argc, char **argv)
+{
+    top = new Vdut;
+    tfp = new VerilatedVcdC;
+
+    Verilated::traceEverOn(true);
+    top->trace(tfp, 99);
+    tfp->open("waveform.vcd");
+
+    testing::InitGoogleTest(&argc, argv);
+    auto res = RUN_ALL_TESTS();
+
+    top->final();
+    tfp->close();
+
+    delete top;
+    delete tfp;
+
+    return res;
+}
