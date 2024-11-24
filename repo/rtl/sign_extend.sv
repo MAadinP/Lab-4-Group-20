9a56e330968c1f8b57a0a1392a97204ca798cd4d
Dawud Khan <dawudakhan0711@gmail.com>
2024-11-24 21:34:30 +0000

    added immsrc and changed to module

--- repo/rtl/sign_extend.sv
+++ repo/rtl/sign_extend.sv
@@ -1,12 +1,11 @@
-module sign_extend (
-    /* verilator lint_off UNUSED */
-    input logic [31:0] instr,       
-    /* verilator lint_on UNUSED */
-      // Input instruction
+module sign_extend #(
+    input logic [31:0] instr,       // Input instruction
+    input logic immsrc,            // 1-bit control signal for immediate source
     output logic [31:0] imm_ext    // Extended immediate
 );
-    always_comb begin
-        imm_ext = {{20{instr[31]}}, instr[31:20]}; // Sign extend 12-bit immediate
-    end
-
+    always_comb 
+        if (immsrc) 
+            imm_ext = {{20{instr[31]}}, instr[31:20]};  // (for I-type)
+        else 
+            imm_ext = {{20{instr[31]}}, instr[11:0]};   // (for S-type)
 endmodule
